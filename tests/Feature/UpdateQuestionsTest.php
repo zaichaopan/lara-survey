7e597cb03db48f46d15d0e9d980d8a7f7ff4f949
zaichaopan <zaichaopan@gmail.com>
2018-01-02 15:19:52 -0800

    Add update question test cases in UpdateQuestionsTest

--- tests/Feature/UpdateQuestionsTest.php
+++ tests/Feature/UpdateQuestionsTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Option;
 use Tests\TestCase;
+use App\OpenSubmittable;
+use App\ScaleSubmittable;
+use App\MultipleChoiceSubmittable;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UpdateQuestionsTest extends TestCase
@@ -31,22 +35,69 @@ class UpdateQuestionsTest extends TestCase
     /** @test */
     public function author_can_update_multiple_choice_question()
     {
+        $question = $this->createQuestion(MultipleChoiceSubmittable::class);
+
+        $question->addOptions([
+            new Option(['text' => 'foo']),
+            new Option(['text' => 'bar']),
+            new Option(['text' => 'baz']),
+        ]);
+
+        $this->updateQuestion($question, [
+            'title' => 'new title',
+            'submittable_type' => 'multiple_choice_submittable',
+            'options' => ['new foo', 'new bar' , 'new baz']
+        ])->assertRedirect(route('surveys.show', ['survey' => $question->survey_id]));
+
+        $question = $question->fresh();
+        $this->assertEquals('new title', $question->title);
+        $this->assertEquals([
+            'new foo', 'new bar', 'new baz'
+        ], $question->options->pluck('text')->all());
     }
 
-    protected function updateQuestion($overrides = [])
+    /** @test */
+    public function author_can_update_open_questions()
+    {
+        $question = $this->createQuestion(OpenSubmittable::class);
+        $this->updateQuestion($question, [
+            'title' => 'new title',
+            'submittable_type' => 'open_submittable',
+        ])->assertRedirect(route('surveys.show', ['survey' => $question->survey_id]));
+        $this->assertEquals('new title', $question->fresh()->title);
+    }
+
+    /** @test */
+    public function author_can_update_scale_questions()
+    {
+        $question = $this->createQuestion(ScaleSubmittable::class);
+        $this->updateQuestion($question, [
+            'title' => 'new title',
+            'submittable_type' => 'scale_submittable',
+            'minimum' => 1,
+            'maximum' => 10
+        ])->assertRedirect(route('surveys.show', ['survey' => $question->survey_id]));
+        $question = $question->fresh();
+        $this->assertEquals('new title', $question->title);
+        $this->assertEquals(1, $question->submittable->minimum);
+        $this->assertEquals(10, $question->submittable->maximum);
+    }
+
+    protected function updateQuestion($question, $data)
+    {
+        return $this->patch(route('surveys.questions.update', [
+            'survey' => $question->survey_id, 'question'=> $question
+        ]), $data);
+    }
+
+    protected function createQuestion($submittableClass)
     {
         $this->login();
         $survey = factory('App\Survey')->create(['user_id' => auth()->id()]);
-        $question = [
-            'title' => 'foo',
-            'submittable_type' => array_random([
-                'multiple_choice_submittable',
-                'open_submittable',
-                'scale_submittable'])
-        ];
-
-        return $this->post(route('surveys.questions.store', [
-            'survey' => $survey,
-        ]), array_merge($question, $overrides));
+        $question = factory('App\Question')->create(['survey_id' => $survey->id]);
+        $submittable = new $submittableClass;
+        $submittable->save();
+        $question = $question->associateType($submittable);
+        return $question;
     }
 }
