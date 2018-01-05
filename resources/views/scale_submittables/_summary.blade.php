efe8c25756d50c94da2ce6110345d5eea3c147b7
zaichaopan <zaichaopan@gmail.com>
2018-01-04 22:05:04 -0800

    Refactor summary from multiple_choice_submittables and scale_submittables to _option_summary

--- resources/views/scale_submittables/_summary.blade.php
+++ resources/views/scale_submittables/_summary.blade.php
@@ -1,5 +1,1 @@
- <ul>
- @foreach($question->summary() as $optionSummary)
-    <li>{{ $optionSummary->option}}: {{$optionSummary->chosenCount}} chosen {{$optionSummary->chosenInPercentage}}</li>
- @endforeach
-</ul>
+@include('summaries._option_summary')
---
+++ resources/views/summaries/_option_summary.blade.php
@@ -0,0 +1,5 @@
+ <ul>
+ @foreach($question->summary() as $optionSummary)
+    <li>{{ $optionSummary->option}}: {{$optionSummary->chosenCount}} chosen {{$optionSummary->chosenInPercentage}}</li>
+ @endforeach
+</ul>
