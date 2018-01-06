8d6a4d6de9e5bb1f2ba1cd7508fb9ead654bd8a7
zaichaopan <zaichaopan@gmail.com>
2018-01-05 20:19:38 -0800

    Remove unused class input in SummariesController

--- app/Http/Controllers/SummariesController.php
+++ app/Http/Controllers/SummariesController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Survey;
-use App\Summarizes\UserAnswer;
-use App\Exception\ClassNotFound;
 
 class SummariesController extends Controller
 {
@@ -15,7 +13,7 @@ class SummariesController extends Controller
 
     public function show(Survey $survey, $summaryStrategy)
     {
-        $summary  = $survey->summary($summaryStrategy);
+        $summary = $survey->summary($summaryStrategy);
         return view('summaries.show', compact('summary'));
     }
 }
